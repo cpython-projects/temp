6e7b2d3f23336520ec318b28dde82007045b94fe
OST <cpython.projects@gmail.com>
2022-01-18 19:46:16 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+a = float(input('a='))
+b = float(input('b='))
+c = float(input('c='))
+
+p = (a + b + c) / 2
+res = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+print(res)
+
+print('Hello')
+
+
+
+
+
+
+
+
